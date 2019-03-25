8715162c34a91afdd98b539232012c1421e63bd7
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-25 14:29:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   squared_array = []
   array.each do |array|
-    squared_array = array ** 2
+    squared_array << array ** 2
   end
   squared_array
 end
